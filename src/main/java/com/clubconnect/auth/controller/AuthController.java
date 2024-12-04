38cf1d2f58d33dd2f0802c8d5799fff25875db1c
bhavindewani99 <bhavindewani@gmail.com>
2024-12-03 18:59:35 -0800

    Added cors

--- src/main/java/com/clubconnect/auth/controller/AuthController.java
+++ src/main/java/com/clubconnect/auth/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200",allowedHeaders = "*")
 public class AuthController {
 
     @Autowired
